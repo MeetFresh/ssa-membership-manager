2621eacf355800971bc45f6f63353613be766bdc
Bingyao Wang <bingyaowang.nj@gmail.com>
2020-08-28 15:45:32 -0400

    payment

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,21 @@ import store from './store'
 import './App.css';
 import Dashboard from './dashboard'
 
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+const promise = loadStripe("pk_test_51HKUBkCT0gTsZJ1O7Z5eOtt5HwxFFk1Mh6RDeVer8wxM5ioHdLohr4HovlxrdDW9SBZIQvSeOX3xbqnK3e4cOQVx00F3k0i43R");
+
 function App() {
     return (
       <div className="App">
-        <Provider store={store}>
+        {/* <Provider store={store}>
           <Dashboard />
-        </Provider>
+        </Provider> */}
+        <Elements stripe={promise}>
+          <CheckoutForm />
+        </Elements>
       </div>
     );
 }
