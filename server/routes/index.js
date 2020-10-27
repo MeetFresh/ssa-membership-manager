3cf71fcc9489a71969220853c8a4a270d23cf652
PhillipFeiDing <fding33@gatech.edu>
2020-10-26 23:45:07 -0400

    add server change

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const user = require('./users.js');
+const event = require('./event.js')
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
@@ -8,5 +9,6 @@ const user = require('./users.js');
 // });
 
 router.use('/user', user);
+router.use('/event', event);
 
 module.exports = router;
